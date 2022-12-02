d8fe2b598cc3a1df3ba641b1fc0ac47b4dc00d05
dearestman <Stupakov.dmitriy1997@gmail.com>
2022-12-02 15:21:12 +0300

    project init

---
+++ src/test/java/ru/stupakov/pricescsi/PricesCsiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.stupakov.pricescsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricesCsiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
